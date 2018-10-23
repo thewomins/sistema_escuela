089efd0f5ce78a523dd8503b46aa4c61bf05fde7
thewomins <thewomins@gmail.com>
2018-10-23 16:14:02 -0300

    inicial

---
+++ alumnos_regulares/alumnos_regulares.cc
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   alumnos_regulares.cc
+ * Author: Felipe
+ * 
+ * Created on 22 de octubre de 2018, 22:45
+ */
+
+#include "alumnos_regulares.h"
+
+alumnos_regulares::alumnos_regulares() {
+}
+
+alumnos_regulares::alumnos_regulares(const alumnos_regulares& orig) {
+}
+
+alumnos_regulares::~alumnos_regulares() {
+}
+
